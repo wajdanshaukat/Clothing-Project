a9f8bfaa922547126e2f430e0e852ac1e6581aac
wajdanshaukat <wajdanshaukat927@gmail.com>
2025-01-10 12:33:44 +0500

    having some code of redux in my header component just rid it.

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useContext } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { auth } from "../firebase/firebaseUtils";
